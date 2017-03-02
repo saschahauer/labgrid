e7f301212242f010b8d89c747a394b6b6786fc26
Sascha Hauer <s.hauer@pengutronix.de>
2017-03-02 09:44:17 +0100

    remote/client: Add -v option for places command
    By default only show a list of available places, with -v give
    the more verbose variant. This behaviour is analog to the
    resources command.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- labgrid/remote/client.py
+++ labgrid/remote/client.py
@@ -155,8 +155,20 @@ class ClientSession(ApplicationSession):
         for name, place in sorted(self.places.items()):
             if self.args.acquired and place.acquired is None:
                 continue
-            print("Place '{}':".format(name))
-            place.show(level=1)
+            if self.args.verbose:
+                print("Place '{}':".format(name))
+                place.show(level=1)
+            else:
+                aliases = []
+                line = "{}".format(name)
+
+                for alias in place.aliases:
+                    aliases.append("{}".format(alias))
+
+                if aliases:
+                    line += " ({})".format(' '.join(aliases))
+
+                print("{0:<40s} {1}".format(line, place.comment))
 
     def _match_places(self, pattern):
         result = set()
@@ -453,6 +465,7 @@ def main():
 
     subparser = subparsers.add_parser('places')
     subparser.add_argument('-a', '--acquired', action='store_true')
+    subparser.add_argument('-v', '--verbose', action='store_true')
     subparser.set_defaults(func=ClientSession.print_places)
 
     subparser = subparsers.add_parser('show', parents=[place_parser],
